b4ad63d85ca70988a66921740196db8d2303d82f
hidari-tate6416 <tate6416@docomo.ne.jp>
2025-04-11 19:35:14 +0900

    ローディングアニメーションの追加

--- components/Index.js
+++ components/Index.js
@@ -1,10 +1,20 @@
 import Link from 'next/link';
 import Header from './parts/Header.js';
 import Footer from './parts/Footer.js';
+import Loading from './parts/Loading.js';
+import { useState, useEffect } from "react";
 
-export default function Index({title, children}) {
+export default function Index({title, loadingFlag = true, children}) {
+
+  const [loadingOn, setLoadingOn] = useState(loadingFlag);
+
+  useEffect(() => {
+    setTimeout(() => setLoadingOn(false), 1500);
+  }, []);
+
   return (
     <div class="bg-jlc-main mx-auto w-full">
+      {(loadingOn) && <Loading />}
       <div class="flex flex-col min-h-screen">
         <Header />
 
