3ab0c0175387c6c12702652720dc13f5eb122642
hidari-tate6416 <tate6416@docomo.ne.jp>
2025-05-07 20:25:07 +0900

    スマホで見たときに画面が拡大されてしまう不具合の修正

--- components/parts/Loading.js
+++ components/parts/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Index() {
   return (
-    <div class="absolute w-full h-full z-20 gap-6 bg-white bg-opacity-80 transition-opacity animate-puff-out-center" role="status" aria-live="polite">
+    <div class="absolute w-full h-full max-h-screen z-20 gap-6 bg-white bg-opacity-80 transition-opacity animate-puff-out-center" role="status" aria-live="polite">
       <div class="flex justify-center items-center h-full" aria-label="読み込み中">
         <div class="animate-ping h-4 w-4 bg-blue rounded-full"></div>
       </div>
